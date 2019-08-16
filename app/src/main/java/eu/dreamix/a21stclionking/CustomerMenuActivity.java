a9b9d1264c3a58ee1ecb771366c5d07296c68471
martin.patsov@dreamix.eu <martin.patsov@dreamix.eu>
2019-08-16 21:12:44 +0300

    Add empty customer menu

---
+++ app/src/main/java/eu/dreamix/a21stclionking/CustomerMenuActivity.java
@@ -0,0 +1,14 @@
+package eu.dreamix.a21stclionking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_menu);
+    }
+}
