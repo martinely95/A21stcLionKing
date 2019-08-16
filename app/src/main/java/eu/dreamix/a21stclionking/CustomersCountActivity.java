334a174fc00d5f21addb7fb014291d5f9b17e829
martin.patsov@dreamix.eu <martin.patsov@dreamix.eu>
2019-08-16 20:39:30 +0300

    Added customers count screen

---
+++ app/src/main/java/eu/dreamix/a21stclionking/CustomersCountActivity.java
@@ -0,0 +1,14 @@
+package eu.dreamix.a21stclionking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomersCountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customers_count);
+    }
+}
