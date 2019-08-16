240307fd5afc0ed179eb9bc86451ee804973605e
martin.patsov@dreamix.eu <martin.patsov@dreamix.eu>
2019-08-16 22:29:47 +0300

    Added dynamic content to customer menu

--- app/src/main/java/eu/dreamix/a21stclionking/CustomerMenuActivity.java
+++ app/src/main/java/eu/dreamix/a21stclionking/CustomerMenuActivity.java
@@ -1,8 +1,11 @@
 package eu.dreamix.a21stclionking;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import static eu.dreamix.a21stclionking.Constants.INITIAL_CUSTOMERS_COUNT;
 
@@ -14,7 +17,35 @@ public class CustomerMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_menu);
-        System.out.println(getIntent().getIntExtra("customersCount", INITIAL_CUSTOMERS_COUNT));
+        customersCount = getIntent().getIntExtra("customersCount", INITIAL_CUSTOMERS_COUNT);
+        System.out.println(customersCount);
+        setRelativeLayout();
+    }
 
+    private void setRelativeLayout() {
+        setContentView(R.layout.activity_customer_menu);
+
+        // Get the widgets reference from XML layout
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.rl);
+
+        // Create a TextView programmatically.
+        TextView tv = new TextView(getApplicationContext());
+
+        // Create a LayoutParams for TextView
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, // Width of TextView
+                RelativeLayout.LayoutParams.WRAP_CONTENT); // Height of TextView
+
+        // Apply the layout parameters to TextView widget
+        tv.setLayoutParams(lp);
+
+        // Set text to display in TextView
+        tv.setText("This is a sample TextView...");
+
+        // Set a text color for TextView text
+        tv.setTextColor(Color.parseColor("#ff0000"));
+
+        // Add newly created TextView to parent view group (RelativeLayout)
+        rl.addView(tv);
     }
 }
