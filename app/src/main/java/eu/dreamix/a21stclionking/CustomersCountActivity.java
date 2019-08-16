4459a7d6478e30289b1605e0d1024e9a669699c4
martin.patsov@dreamix.eu <martin.patsov@dreamix.eu>
2019-08-16 22:05:49 +0300

    Add put extra for propagating customer count

--- app/src/main/java/eu/dreamix/a21stclionking/CustomersCountActivity.java
+++ app/src/main/java/eu/dreamix/a21stclionking/CustomersCountActivity.java
@@ -9,26 +9,32 @@ import android.widget.TextView;
 
 public class CustomersCountActivity extends AppCompatActivity {
 
+    private static int customersCount = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customers_count);
+
+        TextView textView = findViewById(R.id.customerCount);
+        textView.setText(String.valueOf(customersCount));
     }
 
     public void onOkClick(View view) {
         Intent intent = new Intent(".CustomerMenuActivity");
+        intent.putExtra("customerCount", customersCount);
         startActivity(intent);
     }
 
     public void onIncreaseClick(View view) {
         TextView textView = findViewById(R.id.customerCount);
-        int customersCount = Integer.parseInt(textView.getText().toString());
+        customersCount = Integer.parseInt(textView.getText().toString());
         textView.setText(String.valueOf(++customersCount));
     }
 
     public void onDecreaseClick(View view) {
         TextView textView = findViewById(R.id.customerCount);
-        int customersCount = Integer.parseInt(textView.getText().toString());
+        customersCount = Integer.parseInt(textView.getText().toString());
         textView.setText(String.valueOf(--customersCount > 1 ? customersCount : 1));
     }
 
