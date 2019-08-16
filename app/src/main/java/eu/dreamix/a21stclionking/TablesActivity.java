ddadf2eec2dea47810c845de9f11e701462e43ef
martin.patsov@dreamix.eu <martin.patsov@dreamix.eu>
2019-08-16 20:07:39 +0300

    Initial commit empty project.

---
+++ app/src/main/java/eu/dreamix/a21stclionking/TablesActivity.java
@@ -0,0 +1,13 @@
+package eu.dreamix.a21stclionking;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TablesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tables);
+    }
+}
