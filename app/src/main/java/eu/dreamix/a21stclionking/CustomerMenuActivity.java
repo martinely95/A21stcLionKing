ede9ea2f3042359017e17382cc21cf80934cadbb
martin.patsov@dreamix.eu <martin.patsov@dreamix.eu>
2019-08-17 12:13:10 +0300

    Dynamically creating customer labels with relative layout

--- app/src/main/java/eu/dreamix/a21stclionking/CustomerMenuActivity.java
+++ app/src/main/java/eu/dreamix/a21stclionking/CustomerMenuActivity.java
@@ -4,7 +4,7 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -14,6 +14,11 @@ import static eu.dreamix.a21stclionking.Constants.INITIAL_CUSTOMERS_COUNT;
 
 public class CustomerMenuActivity extends AppCompatActivity {
 
+    static final int LEFT_MARGIN = 50;
+    static final int USER_NAME_VERTICAL_MARGIN = 150;
+    static final int FIRST_CUSTOMER_VERTICAL_MARGIN_DECREASE = 110;
+    static final int SIZE = 30;
+    private static final int BUTTON_VERTICAL_MARGIN = 120;
     private int customersCount = 1;
 
     @Override
@@ -28,25 +33,29 @@ public class CustomerMenuActivity extends AppCompatActivity {
     private void setRelativeLayout() {
         setContentView(R.layout.activity_customer_menu);
 
-        LinearLayout layout = new LinearLayout(this);
+        RelativeLayout layout = new RelativeLayout(this);
         setContentView(layout);
-        layout.setOrientation(LinearLayout.VERTICAL);
 
         layout.setBackgroundColor(Color.parseColor("#83C0B448"));
 
+        int topMargin = -FIRST_CUSTOMER_VERTICAL_MARGIN_DECREASE;
         for (int customerIndex = 0; customerIndex < customersCount; customerIndex++) {
-            addTextViewToLayout(layout, "Customer" + (customerIndex + 1));
+            topMargin = addTextViewToLayout(layout, "Customer" + (customerIndex + 1), topMargin);
         }
     }
 
-    private void addTextViewToLayout(ViewGroup layout, String text) {
+    private int addTextViewToLayout(ViewGroup layout, String text, int previousViewTopMargin) {
         // Create a TextView programmatically.
         TextView tv = new TextView(this);
 
         // Create a LayoutParams for TextView
-        ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
                 RelativeLayout.LayoutParams.WRAP_CONTENT, // Width of TextView
                 RelativeLayout.LayoutParams.WRAP_CONTENT); // Height of TextView
+
+        lp.leftMargin = LEFT_MARGIN;
+        previousViewTopMargin = previousViewTopMargin + USER_NAME_VERTICAL_MARGIN;
+        lp.topMargin = previousViewTopMargin;
 
         // Apply the layout parameters to TextView widget
         tv.setLayoutParams(lp);
@@ -59,9 +68,45 @@ public class CustomerMenuActivity extends AppCompatActivity {
 
         tv.setTypeface(null, Typeface.BOLD);
 
-        tv.setTextSize(30);
+        tv.setTextSize(SIZE);
+
+        tv.setLayoutParams(lp);
 
         // Add newly created TextView to parent view group (RelativeLayout)
         layout.addView(tv);
+
+        return addButtonToLayout(layout, "+", previousViewTopMargin);
+    }
+
+    private int addButtonToLayout(ViewGroup layout, String text, int previousViewTopMargin) {
+        // Create a TextView programmatically.
+        Button button = new Button(this);
+
+        // Create a LayoutParams for TextView
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, // Width of TextView
+                RelativeLayout.LayoutParams.WRAP_CONTENT); // Height of TextView
+
+        lp.leftMargin = LEFT_MARGIN;
+        previousViewTopMargin = previousViewTopMargin + BUTTON_VERTICAL_MARGIN;
+        lp.topMargin = previousViewTopMargin;
+
+        // Apply the layout parameters to TextView widget
+        button.setLayoutParams(lp);
+
+        // Set text to display in TextView
+        button.setText(text);
+
+        // Set a text color for TextView text
+        button.setTextColor(Color.parseColor("#000000"));
+
+        button.setTypeface(null, Typeface.BOLD);
+
+        button.setTextSize(SIZE);
+
+        // Add newly created TextView to parent view group (RelativeLayout)
+        layout.addView(button);
+
+        return previousViewTopMargin;
     }
 }
