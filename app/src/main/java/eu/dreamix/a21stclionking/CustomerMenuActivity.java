cf5d7dbddc7be9387ca3787aaaf5546f1fc4b225
martin.patsov@dreamix.eu <martin.patsov@dreamix.eu>
2019-08-17 13:46:52 +0300

    Fix layout for buttons on customer screen

--- app/src/main/java/eu/dreamix/a21stclionking/CustomerMenuActivity.java
+++ app/src/main/java/eu/dreamix/a21stclionking/CustomerMenuActivity.java
@@ -21,7 +21,10 @@ public class CustomerMenuActivity extends AppCompatActivity {
     static final int BUTTON_TEXT_SIZE = 20;
     static final int CUSTOMER_MENU_BUTTON_WIDTH = 120;
     static final int CUSTOMER_MENU_BUTTON_HEIGHT = 120;
-    static final int LEFT_CUSTOMER_BUTTON_MARGIN = CUSTOMER_MENU_BUTTON_WIDTH + 20;
+    static final int SMALL_MARGIN = 20;
+    static final int MEDIUM_MARGIN = 50;
+    static final int LARGE_MARGIN = 80;
+    static final int LEFT_CUSTOMER_BUTTON_MARGIN = CUSTOMER_MENU_BUTTON_WIDTH + SMALL_MARGIN;
     private static final int BUTTON_VERTICAL_MARGIN = 120;
     private int customersCount = 1;
 
@@ -46,8 +49,36 @@ public class CustomerMenuActivity extends AppCompatActivity {
 
         for (int customerIndex = 0; customerIndex < customersCount; customerIndex++) {
             previousMargins.leftMargin = 0;
-            addTextViewToLayout(layout, "Customer" + (customerIndex + 1), previousMargins);
+            addEmptyCustomerToLayout(layout, "Customer" + (customerIndex + 1), previousMargins);
+
+            // Add new customer button
+            previousMargins.leftMargin = 0;
+            addButtonToLayout(layout, "Add Meal", previousMargins);
         }
+
+        // Add new customer button
+        previousMargins.topMargin += LARGE_MARGIN;
+        addButtonToLayoutVertically(layout, "Add Customer", previousMargins);
+    }
+
+    private void addEmptyCustomerToLayout(ViewGroup layout, String text, PreviousMargins previousMargins) {
+        addTextViewToLayout(layout, text, previousMargins);
+
+        // Add delete customer button
+        addButtonToLayoutHorizontally(layout, "X", previousMargins);
+    }
+
+    private void addMenuItemToCustomerOnLayout(ViewGroup layout, String text, PreviousMargins previousMargins) {
+        addTextViewToLayout(layout, text, previousMargins);
+
+        // Add copy item button
+        addButtonToLayoutHorizontally(layout, "*", previousMargins);
+
+        addButtonToLayoutHorizontally(layout, "Y", previousMargins);
+
+        addButtonToLayoutHorizontally(layout, "+", previousMargins);
+
+        addButtonToLayoutHorizontally(layout, "-", previousMargins);
     }
 
     private void addTextViewToLayout(ViewGroup layout, String text, PreviousMargins previousMargins) {
@@ -83,16 +114,6 @@ public class CustomerMenuActivity extends AppCompatActivity {
 
         tv.measure(0, 0);
         previousMargins.leftMargin += tv.getMeasuredWidth();
-
-        addButtonToLayoutHorizontally(layout, "X", previousMargins);
-
-        addButtonToLayoutHorizontally(layout, "Y", previousMargins);
-
-        addButtonToLayoutHorizontally(layout, "+", previousMargins);
-
-        addButtonToLayoutHorizontally(layout, "-", previousMargins);
-
-        addButtonToLayoutVertically(layout, "+", previousMargins);
     }
 
     private void addButtonToLayoutVertically(ViewGroup layout, String text, PreviousMargins previousMargins) {
@@ -119,7 +140,38 @@ public class CustomerMenuActivity extends AppCompatActivity {
 
         button.setTypeface(null, Typeface.BOLD);
 
-        button.setTextSize(TEXT_SIZE);
+        button.setTextSize(BUTTON_TEXT_SIZE);
+
+        // Add newly created TextView to parent view group (RelativeLayout)
+        layout.addView(button);
+    }
+
+    private void addButtonToLayout(ViewGroup layout, String text, PreviousMargins previousMargins) {
+        // Create a TextView programmatically.
+        Button button = new Button(this);
+
+        // Create a LayoutParams for TextView
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, // Width of TextView
+                RelativeLayout.LayoutParams.WRAP_CONTENT); // Height of TextView
+
+        previousMargins.leftMargin += LEFT_CUSTOMER_BUTTON_MARGIN;
+        lp.leftMargin = previousMargins.leftMargin;
+        previousMargins.topMargin += BUTTON_VERTICAL_MARGIN;
+        lp.topMargin = previousMargins.topMargin;
+
+        // Apply the layout parameters to TextView widget
+        button.setLayoutParams(lp);
+
+        // Set text to display in TextView
+        button.setText(text);
+
+        // Set a text color for TextView text
+        button.setTextColor(Color.parseColor("#000000"));
+
+        button.setTypeface(null, Typeface.BOLD);
+
+        button.setTextSize(BUTTON_TEXT_SIZE);
 
         // Add newly created TextView to parent view group (RelativeLayout)
         layout.addView(button);
